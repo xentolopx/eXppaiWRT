1bc8d8722b08880eaf3f109ad3216853f1b5922f
OppaiCyber <30565695+OppaiCyber@users.noreply.github.com>
2022-11-16 00:38:26 +0700

    Update add.sh

--- src/plugins/add.sh
+++ src/plugins/add.sh
@@ -7,5 +7,5 @@ if [ -z $URL]; then
 else
     curl http://127.0.0.1:6800/jsonrpc -X POST --data '{"jsonrpc": "2.0","id":"foo", "method": "aria2.addUri", "params":[["'"$URL"'"]]}' 
     echo "
-Task added to Aria2 - Check download status /aria2stat"
+Task added to Aria2 - Check download status /aria2stats"
 fi
